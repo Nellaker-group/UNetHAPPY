49ce2e9f1c9cb69036cdde676caeab774ecc317d
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2022-03-02 11:46:08 +0000

    can combine folders in google cloud to create one LabelBox dataset

--- labelbox/upload_cloud_images.py
+++ labelbox/upload_cloud_images.py
@@ -1,4 +1,5 @@
 import json
+from typing import List
 
 import typer
 from labelbox import Client, Dataset
@@ -7,7 +8,7 @@ from google.cloud import storage
 
 def main(
     bucket_name: str = typer.Option(...),
-    dir_name: str = typer.Option(...),
+    dir_names: List[str] = typer.Option([]),
     dataset_name: str = typer.Option(...),
     include_polygon_layer: bool = True,
 ):
@@ -17,13 +18,11 @@ def main(
 
     Args:
         bucket_name: Name of the container in google cloud
-        dir_name: Path to directory containing the images you want to link
+        dir_names: Paths to directories containing the images you want to link
         dataset_name: Name of an existing or new dataset in LabelBox
         include_polygon_layer: Include an image layer with drawn polygon
     """
     storage_client = storage.Client()
-    blob_uri = f"gs://{bucket_name}/{dir_name}"
-    blobs = storage_client.list_blobs(bucket_name, prefix=dir_name)
 
     with open("config.json") as f:
         api_key = json.load(f)["LabelBoxAPIKey"]
@@ -39,28 +38,34 @@ def main(
 
     print("Getting image data from Google Cloud...")
     uploads = []
-    for i, image in enumerate(blobs):
-        if image.name != dir_name:
-            image_name = image.name.split(dir_name)[-1]
-            upload_dict = {
-                "external_id": image_name,
-                "row_data": f"{blob_uri}{image_name}",
-                "attachments": [
-                    {
-                        "type": "TEXT",
-                        "value": "Term placenta with no complications",
-                    }
-                ],
-            }
-            if include_polygon_layer:
-                polygon_dir_name = dir_name[:-1]
-                polygon_image_path = (
-                    f"gs://{bucket_name}/{polygon_dir_name}-polygon/{image_name}"
-                )
-                upload_dict["attachments"].append(
-                    {"type": "IMAGE_OVERLAY", "value": polygon_image_path}
-                )
-            uploads.append(upload_dict)
+    for dir_name in dir_names:
+        if include_polygon_layer:
+            dir_name = dir_name[:-1] + "-polygon/"
+        blob_uri = f"gs://{bucket_name}/{dir_name}"
+        blobs = storage_client.list_blobs(bucket_name, prefix=dir_name)
+
+        for image in blobs:
+            if image.name != dir_name:
+                image_name = image.name.split(dir_name)[-1]
+                upload_dict = {
+                    "external_id": image_name,
+                    "row_data": f"{blob_uri}{image_name}",
+                    "attachments": [
+                        {
+                            "type": "TEXT",
+                            "value": "Term placenta with no complications",
+                        }
+                    ],
+                }
+                if include_polygon_layer:
+                    original_image_dir_name = dir_name.split('-polygon/')[-2]
+                    polygon_image_path = (
+                        f"gs://{bucket_name}/{original_image_dir_name}/{image_name}"
+                    )
+                    upload_dict["attachments"].append(
+                        {"type": "IMAGE_OVERLAY", "value": polygon_image_path}
+                    )
+                uploads.append(upload_dict)
     print("Image data obtained!")
 
     print("Linking Images to LabelBox...")
