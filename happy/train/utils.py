7eadcab83e10a3ec78150cfe89e7d5c15f76295e
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2022-03-18 15:04:33 +0000

    cell confusion matrix now adds columns for datasets which do not contain all cell types

--- happy/train/utils.py
+++ happy/train/utils.py
@@ -3,12 +3,29 @@ from datetime import datetime
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 from sklearn.metrics import confusion_matrix
 
 
 def get_confusion_matrix(organ, pred, truth):
     cell_labels = [cell.label for cell in organ.cells]
+    cell_ids = {cell.id for cell in organ.cells}
+
+    unique_values_in_pred = set(pred)
+    unique_values_in_truth = set(truth)
+    unique_values_in_matrix = unique_values_in_pred.union(unique_values_in_truth)
+    missing_cell_ids = list(cell_ids - unique_values_in_matrix)
+    missing_cell_ids.sort()
+
     cm = confusion_matrix(pred, truth)
+
+    if len(missing_cell_ids) > 0:
+        for missing_id in missing_cell_ids:
+            column_insert = np.zeros((cm.shape[0], 1))
+            cm = np.hstack((cm[:,:missing_id], column_insert, cm[:,missing_id:]))
+            row_insert = np.zeros((1, cm.shape[1]))
+            cm = np.insert(cm, missing_id, row_insert, 0)
+
     return pd.DataFrame(cm, columns=cell_labels, index=cell_labels).astype(int)
 
 
