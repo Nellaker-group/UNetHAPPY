f5daa54977549578112ec58307ff38d4905f2883
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2022-06-06 15:26:38 +0100

    kappa between pathologists and myself, then tissue level breakdown of this agreement

--- labelbox/analysis.py
+++ labelbox/analysis.py
@@ -11,6 +11,19 @@ from happy.utils.utils import get_project_dir
 from projects.placenta.results.labelbox.path_mapping import PATHOLOGIST_MAPPING
 
 
+ALL_LABELS = [
+    "terminal_villi",
+    "fibrin",
+    "basal_plate_septa",
+    "mature_intermediary_villi",
+    "stem_villi",
+    "anchoring_villi",
+    "avascular_villi",
+    "villus_sprout",
+    "chorion_amnion",
+]
+
+
 def main(
     file_name: str = typer.Option(...),
     original_file_name: str = typer.Option(...),
@@ -33,12 +46,15 @@ def main(
     with open(path_to_original_subregion_file, "r") as f:
         original_subregion_data = json.load(f)
     original_df = _process_original_data(original_data, original_subregion_data)
+    original_df = _map_to_labelbox_classes(original_df)
 
     # Setup datastructures for analysis bellow
     path_df = df[df["is_pathologist"] == 1]
     nonpath_df = df[df["is_pathologist"] == 0]
     path_predictions = get_images_and_labels_by_person(df, pathologist_only=True)
     nonpath_predictions = get_images_and_labels_by_person(df, pathologist_only=False)
+    path_majority_df = get_majority_labels(path_df)
+    combined_majority_df = combine_path_with_original(path_majority_df, original_df)
 
     # total average time per tissue type
     time_per_tissue = time_per_tissue_type(df)
@@ -58,28 +74,73 @@ def main(
     # counts of tissue types across non experts
     nonpath_tissue_counts = nonpath_df["label"].value_counts()
 
-    # total agreement between pathologists
+    # total kappa between pathologists
     path_kappas = get_total_kappa(path_predictions)
-    print(np.array(path_kappas).mean())
+    print(f"Pathologist's kappa: {np.array(path_kappas).mean():.3f}")
 
-    # TODO: total agreement between pathologists and non-experts
-    # TODO: agreement for each tissue type between pathologists
-    # TODO: agreement for each tissue type between pathologists and non-experts
+    # Total kappa between pathologists and me (removing no pathologist agreement cases)
+    filtered_combined_df = combined_majority_df[
+        combined_majority_df["num_unique"] <= 2
+    ].reset_index(drop=True)
+    original_to_path_kappa = cohen_kappa_score(
+        filtered_combined_df["majority_class"],
+        filtered_combined_df["original_label"],
+    )
+    print(f"Original to path kappa: {original_to_path_kappa:.3f}")
+
+    # agreement for each tissue type between paths and between me and paths
+    for label in ALL_LABELS:
+        label_filtered_df = combined_majority_df[
+            combined_majority_df["majority_class"] == label
+        ]
+        num_images = len(label_filtered_df)
+        if num_images != 0:
+            num_predictions = label_filtered_df["num_unique"].sum()
+            certainty = (num_images / num_predictions) * 100
+            print(
+                f"{label}: images in majority {num_images} | "
+                f"num disagreed {num_predictions - num_images} | "
+                f"certainty {certainty:.1f}%"
+            )
+            original_filtered_df = combined_majority_df[
+                combined_majority_df["original_label"] == label
+            ]
+            num_matching_majority = (
+                original_filtered_df["majority_class"] == label
+            ).sum()
+            non_matching = original_filtered_df[
+                original_filtered_df["majority_class"] != label
+            ]
+            matching_one_mask = (
+                "=" + non_matching["all_preds"].str.join("=") + "="
+            ).str.contains(f"={label}=")
+            num_matching_at_least_one = (
+                len(non_matching[matching_one_mask]) + num_matching_majority
+            )
+            print(
+                f"num of original matching majority: "
+                f"{num_matching_majority}/{num_images} "
+                f"({(num_matching_majority/num_images)*100:.0f}%)"
+            )
+            print(
+                f"num of original matching at least one pathologist: "
+                f"{num_matching_at_least_one}/20 "
+                f"({(num_matching_at_least_one/20)*100:.0f}%)"
+            )
+        else:
+            print(f"{label}: no agreement between pathologists")
+
     # TODO: unusual cases (most disagreement)
-    # TODO: labels marked 'unclear'
+    # TODO: labels marked 'unclear' or 'not_listed'
 
     # Patholgoist distribution of agreement per example (majority of same labels)
-    path_majority_df = get_majority_labels(path_df)
-    plt.figure(figsize=(15, 8))
-    path_majority_df.plot.bar(x='image_name', y='num_unique')
-    plt.savefig(project_dir / "results" / "labelbox" / 'bar_plot.png')
+    # plt.figure(figsize=(15, 8))
+    # path_majority_df.plot.bar(x="image_name", y="num_unique")
+    # plt.savefig(project_dir / "results" / "labelbox" / 'bar_plot.png')
+    # plt.show()
 
-
-    # TODO: total agreement between pathologists and me
-    # TODO: agreement for each tissue type between pathologists and me
-    # TODO: agreement between pathologists but not me
-    # TODO: agreement between pathologists and me
-    # TODO: Hierarchy tissues and agreement based on that...
+    # TODO: plotted agreement between pathologists and me
+    # TODO: Hierarchy tissues and agreement based on that
     # TODO: total agreement between my old qupath self and my current labelbox self
 
     # TODO: plotting of all of this
@@ -120,16 +181,30 @@ def get_total_kappa(all_predictions):
 
 
 def get_majority_labels(df):
-    unique_counts_df = df.groupby("image_name")["label"].nunique().reset_index(drop=False)
+    unique_counts_df = (
+        df.groupby("image_name")["label"].nunique().reset_index(drop=False)
+    )
     unique_counts_df["majority_class"] = (
         df.groupby("image_name")["label"].agg(pd.Series.mode).values
     )
     unique_counts_df.loc[
-        unique_counts_df["majority_class"].apply(lambda x: isinstance(x, np.ndarray))
-    , "majority_class"] = "none"
-    unique_counts_df.columns = ['image_name', 'num_unique', 'majority_class']
-    unique_counts_df = unique_counts_df.sort_values(by=['majority_class'])
+        unique_counts_df["majority_class"].apply(lambda x: isinstance(x, np.ndarray)),
+        "majority_class",
+    ] = "none"
+    unique_counts_df.columns = ["image_name", "num_unique", "majority_class"]
+    unique_counts_df["all_preds"] = (
+        df.groupby("image_name").label.apply(list).reset_index()["label"]
+    )
+    unique_counts_df = unique_counts_df.sort_values(by=["majority_class"])
     return unique_counts_df
+
+
+def combine_path_with_original(path_df, original_df):
+    # Align indicies by image_name for original and grouped path dfs
+    sort_path = path_df.sort_values("image_name")
+    sort_original = original_df.sort_values("image_name").reset_index(drop=True)
+    sort_path["original_label"] = sort_original["label"]
+    return sort_path
 
 
 def _process_labelbox_data(raw_data):
@@ -167,6 +242,22 @@ def _process_original_data(original_raw_data, original_subset_raw_data):
         cleaned_original_subregion_data.append(cleaned_d)
     original_subregion_df = pd.DataFrame(cleaned_original_subregion_data)
     return pd.concat([original_df, original_subregion_df])
+
+
+def _map_to_labelbox_classes(df):
+    label_mapping = {
+        "TVilli": "terminal_villi",
+        "Fibrin": "fibrin",
+        "Maternal": "basal_plate_septa",
+        "MIVilli": "mature_intermediary_villi",
+        "SVilli": "stem_villi",
+        "Avascular": "avascular_villi",
+        "Sprout": "villus_sprout",
+        "AVilli": "anchoring_villi",
+        "Chorion": "chorion_amnion",
+    }
+    df.label = df.label.map(label_mapping)
+    return df
 
 
 if __name__ == "__main__":
