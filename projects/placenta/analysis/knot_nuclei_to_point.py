2ca3bb28156f0482768b390e7753384ce90c6133
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2022-07-01 15:38:25 +0100

    refactored knt post processing code so it can be called in one go from another script

--- projects/placenta/analysis/knot_nuclei_to_point.py
+++ projects/placenta/analysis/knot_nuclei_to_point.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from happy.organs.organs import get_organ
-from happy.utils.utils import get_project_dir
 import happy.db.eval_runs_interface as db
 from happy.hdf5.utils import (
     get_datasets_in_patch,
@@ -26,9 +25,7 @@ def main(
 ):
     # Create database connection
     db.init()
-
     organ = get_organ("placenta")
-    project_dir = get_project_dir(project_name)
 
     # Get path to embeddings hdf5 files
     embeddings_path = get_embeddings_file(project_name, run_id)
@@ -36,33 +33,118 @@ def main(
     all_predictions, all_embeddings, all_coords, all_confidence = get_datasets_in_patch(
         embeddings_path, x_min, y_min, width, height
     )
+
+    _, _, _, _ = process_knt_cells(
+        all_predictions,
+        all_embeddings,
+        all_coords,
+        all_confidence,
+        organ,
+        radius,
+        cut_off_count,
+        width,
+        height,
+        plot=True,
+    )
+
+
+def process_knt_cells(
+    all_predictions,
+    all_embeddings,
+    all_coords,
+    all_confidence,
+    organ,
+    radius,
+    cut_off_count,
+    width,
+    height,
+    plot=False,
+):
     # Filter by KNT cell type
-    all_knt_inds = np.nonzero(all_predictions == 10)[0]
     predictions, embeddings, coords, confidence = filter_by_cell_type(
         all_predictions, all_embeddings, all_coords, all_confidence, "KNT", organ
     )
     print(f"Data loaded with {len(predictions)} KNT cells")
 
+    # Get indices of KNT cells in radius
+    all_knt_inds = np.nonzero(all_predictions == 10)[0]
+    unique_indices = _get_indices_in_radius(coords, radius)
+    if plot:
+        _plot_distances_to_nearest_neighbor([len(x) for x in unique_indices])
+
+    # find KNT cells with no neighbors and turn them into SYN
+    all_predictions, predictions = _convert_isolated_knt_into_syn(
+        all_predictions, predictions, cut_off_count, unique_indices, all_knt_inds
+    )
+
+    # remove points with more than cut off neighbors and keep just one point
+    (
+        predictions,
+        embeddings,
+        coords,
+        confidence,
+        unique_inds_to_remove,
+    ) = _cluster_knts_into_point(
+        predictions, embeddings, coords, confidence, cut_off_count, unique_indices
+    )
+
+    if plot:
+        print(f"Plotting...")
+        visualize_points(
+            organ,
+            "plots/grouped_KNT.png",
+            coords,
+            labels=predictions,
+            width=width,
+            height=height,
+        )
+
+    inds_to_remove_from_total = all_knt_inds[unique_inds_to_remove]
+    all_predictions = np.delete(all_predictions, inds_to_remove_from_total, axis=0)
+    all_embeddings = np.delete(all_embeddings, inds_to_remove_from_total, axis=0)
+    all_coords = np.delete(all_coords, inds_to_remove_from_total, axis=0)
+    all_confidence = np.delete(all_confidence, inds_to_remove_from_total, axis=0)
+
+    return all_predictions, all_embeddings, all_coords, all_confidence
+
+
+def _plot_distances_to_nearest_neighbor(num_in_radius):
+    plt.hist(num_in_radius, bins=100)
+    plt.savefig("plots/num_in_radius_histogram.png")
+    plt.clf()
+
+
+def _get_indices_in_radius(coords, radius):
     tree = sk.KDTree(coords, metric="euclidean")
     all_nn_indices = tree.query_radius(coords, r=radius)
-
     # find indices of duplicate entries and remove duplicates
     unique_indices = np.unique(
         np.array([tuple(row) for row in all_nn_indices], dtype=object)
     )
-    unique_num_in_radius = [len(x) for x in unique_indices]
-    plot_distances_to_nearest_neighbor(unique_num_in_radius)
+    return unique_indices
 
-    # remove KNT cells with no neighbors and turn them into SYN
+
+def _convert_isolated_knt_into_syn(
+    all_predictions, knt_predictions, cut_off_count, unique_indices, all_knt_inds
+):
     lone_knt_indices = []
-    lone_knt_indices_nested = [list(x) for x in unique_indices if len(x) <= cut_off_count]
+    lone_knt_indices_nested = [
+        list(x) for x in unique_indices if len(x) <= cut_off_count
+    ]
     for nested in lone_knt_indices_nested:
         lone_knt_indices.extend(nested)
     all_predictions[all_knt_inds[lone_knt_indices]] = 3
-    predictions[lone_knt_indices] = 3
-    print(f"Converted {len(lone_knt_indices)} KNT cells with few neighbors into SYN")
+    knt_predictions[lone_knt_indices] = 3
+    print(
+        f"Converted {len(lone_knt_indices)} KNT cells with fewer than "
+        f"{cut_off_count+1} neighbors into SYN"
+    )
+    return all_predictions, knt_predictions
 
-    # remove points with more than 4 neighbors and keep just one point
+
+def _cluster_knts_into_point(
+    predictions, embeddings, coords, confidence, cut_off_count, unique_indices
+):
     remaining_inds = [x for x in unique_indices if len(x) > cut_off_count]
     # take all elements except the first to be removed from grouped KNT cells
     inds_to_remove = []
@@ -74,23 +156,10 @@ def main(
 
     # remove clustered
     predictions = np.delete(predictions, unique_inds_to_remove, axis=0)
+    embeddings = np.delete(embeddings, unique_inds_to_remove, axis=0)
     coords = np.delete(coords, unique_inds_to_remove, axis=0)
-
-    print(f"Plotting...")
-    visualize_points(
-        organ,
-        project_dir / "analysis" / "plots" / "grouped_KNT.png",
-        coords,
-        labels=predictions,
-        width=width,
-        height=height,
-    )
-
-
-def plot_distances_to_nearest_neighbor(num_in_radius):
-    plt.hist(num_in_radius, bins=100)
-    plt.savefig("plots/num_in_radius_histogram.png")
-    plt.clf()
+    confidence = np.delete(confidence, unique_inds_to_remove, axis=0)
+    return predictions, embeddings, coords, confidence, unique_inds_to_remove
 
 
 if __name__ == "__main__":
