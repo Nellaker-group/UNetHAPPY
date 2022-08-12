2c8320075be0d673fd4b7958d985734cecdb693f
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2022-08-12 12:21:21 +0200

    analysis for the data extracted from lablebox complete

--- labelbox/analysis.py
+++ labelbox/analysis.py
@@ -6,21 +6,33 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import cohen_kappa_score
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from happy.utils.utils import get_project_dir
 from projects.placenta.results.labelbox.path_mapping import PATHOLOGIST_MAPPING
 
 
-ALL_LABELS = [
-    "terminal_villi",
-    "fibrin",
-    "basal_plate_septa",
-    "mature_intermediary_villi",
-    "stem_villi",
-    "anchoring_villi",
-    "avascular_villi",
+ALL_LABELBOX_LABELS = [
     "villus_sprout",
+    "terminal_villi",
+    "mature_intermediary_villi",
+    "anchoring_villi",
+    "stem_villi",
     "chorion_amnion",
+    "basal_plate_septa",
+    "fibrin",
+    "avascular_villi",
+]
+MODEL_LABELS = [
+    "Sprout",
+    "TVilli",
+    "MIVilli",
+    "AVilli",
+    "SVilli",
+    "Chorion",
+    "Maternal",
+    "Fibrin",
+    "Avascular",
 ]
 
 
@@ -78,7 +90,7 @@ def main(
     path_kappas = get_total_kappa(path_predictions)
     print(f"Pathologist's kappa: {np.array(path_kappas).mean():.3f}")
 
-    # Total kappa between pathologists and me (removing no pathologist agreement cases)
+    # Total kappa between pathologists and me when they are in agreement
     filtered_combined_df = combined_majority_df[
         combined_majority_df["num_unique"] <= 2
     ].reset_index(drop=True)
@@ -86,79 +98,177 @@ def main(
         filtered_combined_df["majority_class"],
         filtered_combined_df["original_label"],
     )
-    print(f"Original to path kappa: {original_to_path_kappa:.3f}")
+    print(f"Original to path majority kappa: {original_to_path_kappa:.3f}")
 
-    for label in ALL_LABELS:
-        label_filtered_df = combined_majority_df[
-            combined_majority_df["majority_class"] == label
-        ]
+    for label in ALL_LABELBOX_LABELS:
+        # kappa per tissue type as one vs rest per pathologist combination average
+        mean_label_kappa = kappa_for_one_tissue(path_predictions, label)
+        print(f"{label} avg kappa: {mean_label_kappa:.3f}")
+
+        label_majority_mask = combined_majority_df["majority_class"] == label
+        label_filtered_df = combined_majority_df[label_majority_mask]
         num_images = len(label_filtered_df)
-        # agreement/certainty for each tissue type between pathologists
         if num_images != 0:
-            num_predictions = label_filtered_df["num_unique"].sum()
-            other_majority_df = combined_majority_df[
-                combined_majority_df["majority_class"] != label
-            ]
-            other_majority_df_disagreement = other_majority_df[
-                other_majority_df["num_unique"] != 1
-            ]
-            num_label_in_other_majority = (
-                other_majority_df_disagreement["all_preds"]
-                .apply(lambda x: x.count(label))
-                .sum()
+            # custom certainty for each tissue type between pathologists
+            path_prediction_stats(
+                combined_majority_df, label_filtered_df, num_images, label
             )
-            certainty = (
-                num_images / (num_predictions + num_label_in_other_majority)
-            ) * 100
-            print(
-                f"{label}: images in majority {num_images} | "
-                f"num disagreed with majority {num_predictions - num_images} | "
-                f"num labels in other majorities {num_label_in_other_majority} | "
-                f"certainty {certainty:.1f}%"
+
+            # agreement for each tissue type between original and pathologists
+            original_label_mask = combined_majority_df["original_label"] == label
+            original_path_kappa = cohen_kappa_score(
+                label_majority_mask, original_label_mask
             )
-            # agreement for each tissue type between me and pathologists
-            original_filtered_df = combined_majority_df[
-                combined_majority_df["original_label"] == label
-            ]
-            num_matching_majority = (
-                original_filtered_df["majority_class"] == label
-            ).sum()
-            non_matching = original_filtered_df[
-                original_filtered_df["majority_class"] != label
-            ]
-            matching_one_mask = (
-                "=" + non_matching["all_preds"].str.join("=") + "="
-            ).str.contains(f"={label}=")
-            num_matching_at_least_one = (
-                len(non_matching[matching_one_mask]) + num_matching_majority
-            )
-            print(
-                f"num of original matching majority: "
-                f"{num_matching_majority}/{num_images} "
-                f"({(num_matching_majority/num_images)*100:.0f}%)"
-            )
-            print(
-                f"num of original matching at least one pathologist: "
-                f"{num_matching_at_least_one}/20 "
-                f"({(num_matching_at_least_one/20)*100:.0f}%)"
-            )
+            print(f"{label} original to path kappa: {original_path_kappa:.3f}")
+
+            # agreement original vs pathologists stats
+            original_vs_path_stats(combined_majority_df, label, num_images)
         else:
             print(f"{label}: no agreement between pathologists")
 
-    # TODO: unusual cases (most disagreement)
-    # TODO: labels marked 'unclear' or 'not_listed'
+    # Most commonly labelled together tissue types
+    pathologist_confusion(combined_majority_df)
 
-    # Patholgoist distribution of agreement per example (majority of same labels)
-    # plt.figure(figsize=(15, 8))
-    # path_majority_df.plot.bar(x="image_name", y="num_unique")
-    # plt.savefig(project_dir / "results" / "labelbox" / 'bar_plot.png')
-    # plt.show()
+    # Pathologist votes against original 'ground truth' confusion matrix
+    original_to_path_confusion(combined_majority_df)
 
-    # TODO: plotted agreement between pathologists and me
-    # TODO: Hierarchy tissues and agreement based on that
-    # TODO: total agreement between my old qupath self and my current labelbox self
 
-    # TODO: plotting of all of this
+def kappa_for_one_tissue(path_predictions, label):
+    indices = list(range(len(path_predictions)))
+    indices_combinations = list(combinations(indices, 2))
+    all_kappas = []
+    for comb in indices_combinations:
+        label_map_1 = path_predictions[comb[0]]["label"] == label
+        label_map_2 = path_predictions[comb[1]]["label"] == label
+        kappa = cohen_kappa_score(label_map_1, label_map_2)
+        if np.isnan(kappa):
+            kappa = 0
+        all_kappas.append(kappa)
+    mean_label_kappa = np.array(all_kappas).mean()
+    return mean_label_kappa
+
+
+def path_prediction_stats(combined_majority_df, label_filtered_df, num_images, label):
+    num_predictions = label_filtered_df["num_unique"].sum()
+    other_majority_df = combined_majority_df[
+        combined_majority_df["majority_class"] != label
+    ]
+    other_majority_df_disagreement = other_majority_df[
+        other_majority_df["num_unique"] != 1
+    ]
+    num_label_in_other_majority = (
+        other_majority_df_disagreement["all_preds"]
+        .apply(lambda x: x.count(label))
+        .sum()
+    )
+    certainty = (num_images / (num_predictions + num_label_in_other_majority)) * 100
+    print(
+        f"{label} images in majority {num_images} | "
+        f"num disagreed with majority {num_predictions - num_images} | "
+        f"num labels in other majorities {num_label_in_other_majority} | "
+        f"certainty {certainty:.1f}%"
+    )
+
+
+def original_vs_path_stats(combined_majority_df, label, num_images):
+    num_pathologists = len(combined_majority_df["all_preds"][0])
+    original_filtered_df = combined_majority_df[
+        combined_majority_df["original_label"] == label
+    ]
+    num_matching_majority = (original_filtered_df["majority_class"] == label).sum()
+    count_matching = (
+        "==" + original_filtered_df["all_preds"].str.join("==") + "=="
+    ).str.count(f"={label}=")
+    print(
+        f"num of original images matching majority image: "
+        f"{num_matching_majority}/{num_images} "
+        f"({(num_matching_majority / num_images) * 100:.0f}%)"
+    )
+    print(
+        f"num of original matching at least one pathologist: "
+        f"{(count_matching >= 1).sum()}/20 "
+        f"({((count_matching >= 1).sum() / 20) * 100:.0f}%)"
+    )
+    print(
+        f"num of original matching at least two pathologist: "
+        f"{(count_matching >= 2).sum()}/20 "
+        f"({((count_matching >= 2).sum() / 20) * 100:.0f}%)"
+    )
+    print(
+        f"num of original matching at least three pathologist: "
+        f"{(count_matching >= 3).sum()}/20 "
+        f"({((count_matching >= 3).sum() / 20) * 100:.0f}%)"
+    )
+    print(
+        f"num of original matching num of total pathologist votes: "
+        f"{count_matching.sum()}/{20 * num_pathologists} "
+        f"({count_matching.sum() / (20 * num_pathologists) * 100:.0f}%)"
+    )
+
+
+def pathologist_confusion(combined_majority_df):
+    labelbox_labels = ALL_LABELBOX_LABELS + ["not_listed", "unclear"]
+    model_labels = MODEL_LABELS + ["Not Listed", "Unclear"]
+    cm = np.empty((len(labelbox_labels), len(labelbox_labels)))
+    for i, label in enumerate(labelbox_labels):
+        label_majority_mask = combined_majority_df["majority_class"] == label
+        label_filtered_df = combined_majority_df[label_majority_mask]
+        for j, other_label in enumerate(labelbox_labels):
+            num_matching = (
+                label_filtered_df["all_preds"]
+                .apply(lambda x: x.count(other_label))
+                .sum()
+            )
+            cm[i, j] = num_matching
+    row_labels = []
+    unique_counts = cm.sum(axis=1)
+    total_counts = cm.sum()
+    label_proportions = ((unique_counts / total_counts) * 100).round(2)
+    for i, label in enumerate(model_labels):
+        row_labels.append(f"{label} ({label_proportions[i]}%)")
+    unique_counts = cm.sum(axis=1)
+    cm_df = (
+        pd.DataFrame(
+            cm / unique_counts[:, None], columns=model_labels, index=model_labels
+        )
+        .fillna(0)
+        .astype(float)
+    )
+    plt.figure(figsize=(10, 8))
+    sns.set(font_scale=1.1)
+    plt.rcParams["figure.dpi"] = 600
+    ax = sns.heatmap(
+        cm_df, annot=True, cmap="Blues", square=True, cbar=False, fmt=".2f"
+    )
+    ax.hlines([9], *ax.get_xlim())
+    ax.vlines([9], *ax.get_ylim())
+    plt.ylabel("Majority Vote")
+    plt.xlabel("Proportion of Votes")
+    plt.tight_layout()
+    plt.savefig("pathologist_confusion.png")
+
+
+def original_to_path_confusion(combined_majority_df):
+    cm = np.empty((len(ALL_LABELBOX_LABELS), len(ALL_LABELBOX_LABELS)))
+    for i, label in enumerate(ALL_LABELBOX_LABELS):
+        label_original_mask = combined_majority_df["original_label"] == label
+        label_filtered_df = combined_majority_df[label_original_mask]
+        for j, other_label in enumerate(ALL_LABELBOX_LABELS):
+            num_matching = (
+                label_filtered_df["all_preds"]
+                .apply(lambda x: x.count(other_label))
+                .sum()
+            )
+            cm[i, j] = num_matching
+    cm_df = pd.DataFrame(cm, columns=MODEL_LABELS, index=MODEL_LABELS)
+    plt.figure(figsize=(10, 8))
+    plt.rcParams["figure.dpi"] = 600
+    sns.heatmap(cm_df, annot=True, cmap="Blues", square=True, cbar=False, fmt="g")
+    plt.yticks(rotation=0)
+    plt.ylabel("Original Tissue Structure Annotation")
+    plt.xlabel("Number of Pathologist Votes")
+    plt.tight_layout()
+    plt.savefig("original_vs_pathologist_confusion.png")
 
 
 def time_per_tissue_type(df):
@@ -262,15 +372,15 @@ def _process_original_data(original_raw_data, original_subset_raw_data):
 
 def _map_to_labelbox_classes(df):
     label_mapping = {
-        "TVilli": "terminal_villi",
-        "Fibrin": "fibrin",
-        "Maternal": "basal_plate_septa",
-        "MIVilli": "mature_intermediary_villi",
-        "SVilli": "stem_villi",
-        "Avascular": "avascular_villi",
         "Sprout": "villus_sprout",
+        "TVilli": "terminal_villi",
+        "MIVilli": "mature_intermediary_villi",
         "AVilli": "anchoring_villi",
+        "SVilli": "stem_villi",
         "Chorion": "chorion_amnion",
+        "Maternal": "basal_plate_septa",
+        "Fibrin": "fibrin",
+        "Avascular": "avascular_villi",
     }
     df.label = df.label.map(label_mapping)
     return df
