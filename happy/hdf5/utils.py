52dc509dc86fbc3271cebf20ec9b855affdfdca4
Jonathan Campbell <zse661@rescomp1.hpc.in.bmrc.ox.ac.uk>
2022-08-31 15:29:24 +0100

    Hack to get it to only run the files I have

--- happy/hdf5/utils.py
+++ happy/hdf5/utils.py
@@ -15,7 +15,11 @@ def get_embeddings_file(project_name, run_id):
         / "results"
         / "embeddings"
     )
-    embeddings_path = db.get_embeddings_path(run_id, embeddings_dir)
+    if run_id == 56 or run_id == 58:
+        return embeddings_dir / f"run_{run_id}.hdf5"
+    else:
+        raise ValueError(f"run_id {run_id} is not valid")
+    # embeddings_path = db.get_embeddings_path(run_id, embeddings_dir)
     return embeddings_dir / embeddings_path
 
 
