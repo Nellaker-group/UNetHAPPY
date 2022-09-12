0bcaf47cbf25b830e33a83980d1d8b0ba925d28b
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2022-09-12 08:35:12 +0100

    removed batch norm from shadow, it made performance a lot worse

--- happy/models/shadow.py
+++ happy/models/shadow.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import SAGEConv, global_mean_pool, norm
+from torch_geometric.nn import SAGEConv, global_mean_pool
 
 
 class ShaDowGCN(torch.nn.Module):
@@ -9,19 +9,15 @@ class ShaDowGCN(torch.nn.Module):
         super().__init__()
         self.num_layers = num_layers
         self.convs = nn.ModuleList()
-        self.bns = nn.ModuleList()
 
         for i in range(num_layers):
             in_channels = in_channels if i == 0 else hidden_channels
             self.convs.append(SAGEConv(in_channels, hidden_channels))
-            self.bns.append(norm.BatchNorm(hidden_channels))
         self.lin = torch.nn.Linear(2 * hidden_channels, out_channels)
 
     def forward(self, x, edge_index, batch, root_n_id):
         for i, conv in enumerate(self.convs):
-            x = conv(x, edge_index)
-            x = self.bns[i](x)
-            x = F.relu(x)
+            x = conv(x, edge_index).relu()
             x = F.dropout(x, p=0.5, training=self.training)
         # We merge both central node embeddings and subgraph embeddings:
         x = torch.cat([x[root_n_id], global_mean_pool(x, batch)], dim=-1)
