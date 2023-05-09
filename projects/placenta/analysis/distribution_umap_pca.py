f0b0bcf35add798628df8f3744ae979df903aba3
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2023-05-09 17:23:08 +0100

    pca plots can check against an external filer with more features to colour the plot

--- projects/placenta/analysis/distribution_umap_pca.py
+++ projects/placenta/analysis/distribution_umap_pca.py
@@ -6,6 +6,7 @@ from bokeh.plotting import show, save, output_file
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.decomposition import PCA
+import numpy as np
 
 from happy.utils.utils import get_project_dir
 
@@ -13,7 +14,8 @@ from happy.utils.utils import get_project_dir
 def main(
     project_name: str = typer.Option(...),
     csv_dir: str = typer.Option(...),
-    plot_name: str = typer.Option(...),
+    features_data_csv: str = "",
+    umap_plot_name: str = "",
     value_of_interest: str = typer.Option(...),
     include_cells: bool = True,
     include_tissues: bool = False,
@@ -29,8 +31,9 @@ def main(
     Args:
         project_name: name of the project dir to save to
         csv_dir: path from project dir to csvs with data
-        plot_name: name of html file to save to
-        value_of_interest: name of the column for colouring the UMAP
+        features_data_csv: name of csv with additional features data
+        umap_plot_name: name of html file to save to
+        value_of_interest: name of the column for colouring the plot points
         include_cells: whether to include cell data
         include_tissues: whether to include tissue data
         include_proportions: whether to include proportion data
@@ -66,18 +69,52 @@ def main(
         df = df.merge(tissue_counts, on="run_id")
     if include_tissues and include_proportions:
         df = df.merge(tissue_proportions, on="run_id")
-    df_features = df.drop(columns=["run_id"])
 
     # Prepare data for plotting colours
     ct_prop = cell_proportions.merge(tissue_proportions, on="run_id")
-    ct_prop = ct_prop.drop(columns=["run_id"])
-    data_of_interest = ct_prop[value_of_interest]
-
-    if value_of_interest == "Total":
+    cell_names = cell_proportions.columns[1:]
+    tissue_names = tissue_proportions.columns[1:]
+    if value_of_interest in cell_names or value_of_interest in tissue_names:
+        data_of_interest = ct_prop[value_of_interest]
+    elif value_of_interest == "Total":
         ct_counts = cell_counts.merge(tissue_counts, on="run_id")
         ct_counts["Total"] = cell_counts["Total"]
         ct_counts = ct_counts.drop(columns=["run_id"])
         data_of_interest = ct_counts[value_of_interest]
+    else:
+        plotting_features_df = pd.read_csv(data_dir / features_data_csv)
+        if value_of_interest == "trimester":
+            data_of_interest = pd.cut(
+                plotting_features_df["gestational_week"],
+                [-2, 0, 13, 29, 40, np.inf],
+                labels=[
+                    "unknown",
+                    "1st_trimester",
+                    "2nd_trimester",
+                    "3rd_trimester",
+                    "term",
+                ],
+            )
+        elif "diagnosis" in value_of_interest:
+            column_name = value_of_interest.split("_")[:2]
+            column_name = "_".join(column_name)
+            lesion = value_of_interest.split("_")[2:]
+            lesion = "_".join(lesion)
+            column_of_interest = plotting_features_df[column_name]
+            data_of_interest = column_of_interest.str.contains(lesion).map(
+                {True: lesion, False: ""}
+            )
+            data_of_interest = data_of_interest.fillna("")
+        elif value_of_interest == "slide_name":
+            data_of_interest = plotting_features_df["slide_name"]
+            data_of_interest = data_of_interest.apply(lambda x: len(x) > 10).map(
+                {True: "Estonia", False: "Israel"}
+            )
+        else:
+            raise ValueError(f"Unknown value of interest: {value_of_interest}")
+
+    df_features = df.drop(columns=["run_id"])
+    ct_prop = ct_prop.drop(columns=["run_id"])
 
     if not pca:
         # Generate UMAP
@@ -92,10 +129,10 @@ def main(
             point_size=5,
         )
 
-        output_file(data_dir / plot_name, title=f"UMAP for {value_of_interest}")
+        output_file(data_dir / umap_plot_name, title=f"UMAP for {value_of_interest}")
         show(plot)
-        print(f"saving interactive to {data_dir / plot_name}")
-        save(plot, data_dir / plot_name)
+        print(f"saving interactive to {data_dir / umap_plot_name}")
+        save(plot, data_dir / umap_plot_name)
     else:
         sns.set()
         # Generate PCA
@@ -111,8 +148,8 @@ def main(
             plt.title("Explained Variance")
             plt.show()
 
-        # Plot PCA
-        data_dir = data_dir / 'pca'
+        # Setup directory and plot name
+        data_dir = data_dir / "pca"
         if include_cells and not include_tissues:
             feature_type = "Cell"
             data_dir = data_dir / "cell_features"
@@ -129,16 +166,22 @@ def main(
         ct_counts = ct_counts.drop(columns=["run_id"])
 
         # Plot PCA coloured by each structure
-        for structure in ct_prop.columns:
-            data_of_interest = ct_prop[structure]
-            plot_pca(pca_df, feature_type, data_of_interest, structure, data_dir)
-        data_of_interest = ct_counts["Total"]
-        plot_pca(pca_df, feature_type, data_of_interest, "Total", data_dir)
+        if features_data_csv is "":
+            for structure in ct_prop.columns:
+                data_of_interest = ct_prop[structure]
+                plot_pca(pca_df, feature_type, data_of_interest, structure, data_dir)
+            data_of_interest = ct_counts["Total"]
+            plot_pca(pca_df, feature_type, data_of_interest, "Total", data_dir)
+        else:
+            plot_pca(
+                pca_df, feature_type, data_of_interest, value_of_interest, data_dir
+            )
 
 
 # Plot PCA
 def plot_pca(pca_df, feature_type, data_of_interest, value_of_interest, data_dir):
     sns.scatterplot(data=pca_df, x=0, y=1, hue=data_of_interest)
+    # sns.scatterplot(data=pca_df, x=0, y=1, style=data_of_interest)
     plt.title(f"PCA for {value_of_interest} on {feature_type} features")
     value_name = value_of_interest.replace(" ", "_").replace("/", "_")
     plt.savefig(data_dir / f"{feature_type}_{value_name}_pca.png")
