b6404ff8e88d2f839f3980402e938e3bdf50f67e
e-jorsboe <jorsboe@hotmail.com>
2023-06-06 14:06:05 +0100

    added option for filtering like in Craig's paper

--- projects/adipose/emilScripts/generateMeanAdipocyte.py
+++ projects/adipose/emilScripts/generateMeanAdipocyte.py
@@ -25,13 +25,16 @@ from shapely.geometry import Polygon, MultiPolygon, shape  # (pip install Shapel
 prs = argparse.ArgumentParser()
 prs.add_argument('--i1', help='Start index (included)', type=int)
 prs.add_argument('--i2', help='End index (included)', type=int)
+prs.add_argument('--craigFilter', help='Whether to apply filtering like Craig', type=bool, default=False)
 args = vars(prs.parse_args())
+
 
 assert args['i1'] != ""
 assert args['i2'] != ""
 
 i1 = args['i1']
 i2 = args['i2']
+craigFilter = args['craigFilter']
 
 
 #################################################################
@@ -110,7 +113,11 @@ for j in range(i1,i2+1):
             whichPixel = "endox"
 
         for poly in polys:
-            if poly.area*pixels[whichPixel]**2 >= 200 and ((4*math.pi*poly.area ) / ((poly.length)**2)) > 0.75:
+            if craigFilter:
+                good = poly.area*pixels[whichPixel]**2 >= 200 and poly.area*pixels[whichPixel]**2 <= 16000
+            else:
+                good = poly.area*pixels[whichPixel]**2 >= 200 and ((4*math.pi*poly.area ) / ((poly.length)**2)) > 0.75
+            if good:
                 tmp_list.append(poly.area*pixels[whichPixel]**2)
                 if poly.area*pixels[whichPixel]**2 < 750:
                     below750_list.append(poly.area*pixels[whichPixel]**2)
@@ -131,7 +138,7 @@ for j in range(i1,i2+1):
     df3 = pd.DataFrame.from_dict(merged_dict_N, orient="index", columns=["Nadipocytes"])
     df4 = pd.DataFrame.from_dict(merged_dict_below750, orient="index", columns=["fracBelow750"])
     dfTotal=pd.concat([df['avg'],df2['stdev'],df3['Nadipocytes'],df4['fracBelow750']],axis=1)
-    dfTotal.to_csv("multiRun_Total_from"+str(i1)+"_to"+str(i2)+"V2.csv")
+    dfTotal.to_csv("multiRun_Total_from"+str(i1)+"_to"+str(i2)+"_craig"+str(craigFilter)+"V2.csv")
 
 
 
