7ff6068fe10ddb3e5cce749729df2d9d29f47cb1
e-jorsboe <jorsboe@hotmail.com>
2023-09-08 14:07:27 +0100

    added new filter for extracting polygons and plotting them and making geojsons

--- projects/adipose/emilScripts/geojson2statsV2.py
+++ projects/adipose/emilScripts/geojson2statsV2.py
@@ -32,28 +32,49 @@ res = readGeoJSON2list(filename)
 
 pp_filter_all = []
 area_filter_all = []
-area_filter_in = []
+area_filter_in_old = []
+area_filter_in_new = []
 
 for poly in res:
     area_filter_all.append(poly.area*pixelSize**2)
     pp_filter_all.append((4*math.pi*poly.area ) / ((poly.length)**2))
     if poly.area*pixelSize**2 >= 200 and ((4*math.pi*poly.area ) / ((poly.length)**2)) > 0.75:
-        area_filter_in.append(poly.area*pixelSize**2)
+        area_filter_in_old.append(poly.area*pixelSize**2)
+    if poly.area*pixelSize**2 >= 10**2.5 and ((4*math.pi*poly.area ) / ((poly.length)**2)) > 0.6:
+        area_filter_in_new.append(poly.area*pixelSize**2)
 
 
 
 bins = np.linspace(200, 20000, 100)
 
-fig, axs = pyplot.subplots(2)
-fig.suptitle("merged-size: mean="+str(round(stats.mean(area_filter_all),2))+", med="+str(round(stats.median(area_filter_all),2))+", sd="+str(round(stats.stdev(area_filter_all),2))+", N="+str(len(area_filter_all))+"\n"
-             "size: mean="+str(round(stats.mean(area_filter_in),2))+", med="+str(round(stats.median(area_filter_in),2))+", sd="+str(round(stats.stdev(area_filter_in),2))+", N="+str(len(area_filter_in)))
-axs[0].hist(area_filter_all, bins, alpha=0.5, color="black")
-axs[1].hist(area_filter_in, bins, alpha=0.5, color="black")
-axs[0].set_ylabel('counts')
-axs[1].set_xlabel('size (micrometers**2)')
-axs[1].set_ylabel('counts')
-fig.savefig(filename.replace(".geojson","")+"_size_hist.png")
-fig.clf()
+pyplot.figure(figsize=(14, 8))
+
+
+pyplot.subplot(1, 2, 1)  # First subplot
+
+pyplot.hist(area_filter_all, bins, alpha=0.5, color="black")
+
+pyplot.title("before filter: mean="+str(round(stats.mean(area_filter_all),2))+", med="+str(round(stats.median(area_filter_all),2))+", sd="+str(round(stats.stdev(area_filter_all),2))+", N="+str(len(area_filter_all)))
+pyplot.ylabel('counts')
+pyplot.xlabel('size (micrometers**2)')
+
+pyplot.subplot(1, 2, 2)  # Second subplot
+
+pyplot.hist(area_filter_in_new, bins, alpha=0.5, color="grey")
+pyplot.hist(area_filter_in_old, bins, alpha=0.5, color="grey")
+pyplot.ylabel('counts')
+pyplot.xlabel('size (micrometers**2)')
+
+
+pyplot.title("after old filter (black):  mean="+str(round(stats.mean(area_filter_in_old),2))+", med="+str(round(stats.median(area_filter_in_old),2))+", sd="+str(round(stats.stdev(area_filter_in_old),2))+", N="+str(len(area_filter_in_old))+"\n"     
+          "after new filter (grey):  mean="+str(round(stats.mean(area_filter_in_new),2))+", med="+str(round(stats.median(area_filter_in_new),2))+", sd="+str(round(stats.stdev(area_filter_in_new),2))+", N="+str(len(area_filter_in_new)))
+
+# Adjust spacing between subplots
+pyplot.tight_layout()
+
+
+pyplot.savefig(filename.replace(".geojson","")+"_size_hist.png")
+pyplot.clf()
 
 bins = np.linspace(0, 1, 100)
 
@@ -68,7 +89,9 @@ pyplot.clf()
 print("ALL:")
 print("mean="+str(round(stats.mean(area_filter_all),2))+", med="+str(round(stats.median(area_filter_all),2))+", sd="+str(round(stats.stdev(area_filter_all),2))+", N="+str(len(area_filter_all)))
 
-print("After filter:")
-print("mean="+str(round(stats.mean(area_filter_in),2))+", med="+str(round(stats.median(area_filter_in),2))+", sd="+str(round(stats.stdev(area_filter_in),2))+", N="+str(len(area_filter_in)))
+print("After old filter (size > 200, PP > 0.75):")
+print("mean="+str(round(stats.mean(area_filter_in_old),2))+", med="+str(round(stats.median(area_filter_in_old),2))+", sd="+str(round(stats.stdev(area_filter_in_old),2))+", N="+str(len(area_filter_in_old)))
 
+print("After new filter (size > 316.23, PP > 0.6):")
+print("mean="+str(round(stats.mean(area_filter_in_new),2))+", med="+str(round(stats.median(area_filter_in_new),2))+", sd="+str(round(stats.stdev(area_filter_in_new),2))+", N="+str(len(area_filter_in_new)))
 
